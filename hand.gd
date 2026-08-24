b1087c3ccd3dd2340b07b6f9aa1821636c9165de
Joel Swanson <jswanson993@gmail.com>
2026-08-23 19:48:25 -0600

    Add networking to dealing damage

--- hand.gd
+++ hand.gd
@@ -12,7 +12,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_body_entered(body: Node2D) -> void:
-	if is_punching and body != get_parent():
-		body.take_damage(DAMAGE)
+	if is_punching and body != get_parent() and multiplayer.is_server():
+		request_deal_damage_server.rpc_id(1, body)
+		
+
+@rpc("authority", "call_local", "reliable")
+func request_deal_damage_server(body: Node2D):
+	body.take_damage(DAMAGE)
