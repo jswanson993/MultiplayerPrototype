dfb00aa1f5cca6bdad6fdd6eb49e35b9844f460a
Joel <jswanson993@gmail.com>
2026-08-27 16:07:17 -0600

    Add game mode trigger node

---
+++ game_mode_trigger.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_parent().on_pickup.connect("on_pickup_triggered")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func on_pickup_triggered():
+	print_debug("triggered")
